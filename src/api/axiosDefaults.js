fe4e88c2c394c35a69374f1c4e44f092dc719a24
Ben Butler <benbutleranimator@gmail.com>
2024-05-22 15:34:31 +0000

    Add signup form

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://bezabu-drf-api-f2959062e96d.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
