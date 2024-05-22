64fcc305b57911f6b58f5e6f88479d542b1d748f
Ben Butler <benbutleranimator@gmail.com>
2024-05-22 11:52:35 +0000

    Add NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import Button from "react-bootstrap/Button"
-import './App.css';
+import "./App.css";
+import NavBar from "./components/NavBar"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <Button variant="primary">Primary</Button>
-      </header>
+      <NavBar />
     </div>
   );
 }
