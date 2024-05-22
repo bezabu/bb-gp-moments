2dcd3b571ff0acdb432f2c56e1dea4e215d3a2a3
Ben Butler <benbutleranimator@gmail.com>
2024-05-22 11:35:16 +0000

    Add bootstrap button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
+import Button from "react-bootstrap/Button"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello, World!</h1>
+      <Button variant="primary">Primary</Button>
       </header>
     </div>
   );
