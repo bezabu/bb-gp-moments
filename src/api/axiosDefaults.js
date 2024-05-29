3e7a83932649e59bb713a8d7f0c5668a6c692e35
Ben Butler <benbutleranimator@gmail.com>
2024-05-29 16:29:50 +0000

    Refactor custom burger toggle

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,8 @@ import axios from "axios";
 axios.defaults.baseURL = "https://bezabu-drf-api-f2959062e96d.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
+
