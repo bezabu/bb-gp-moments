f1c7b5a8aa75a8df83c0cdfd347ab12bece505e5
Ben Butler <benbutleranimator@gmail.com>
2024-06-02 15:12:15 +0000

    fix typo

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -42,6 +42,22 @@ const Post = (props) => {
         }
       };
 
+      const handleUnlike = async () => {
+        try {
+          await axiosRes.delete(`/likes/${like_id}/`);
+          setPosts((prevPosts) => ({
+            ...prevPosts,
+            results: prevPosts.results.map((post) => {
+              return post.id === id
+                ? { ...post, likes_count: post.likes_count - 1, like_id: null }
+                : post;
+            }),
+          }));
+        } catch (err) {
+          console.log(err);
+        }
+      };
+
   return (
     <Card className={styles.Post}>
         <Card.Body>
@@ -68,11 +84,11 @@ const Post = (props) => {
                         <i className='far fa-heart' />
                     </OverlayTrigger>
                 ) : like_id ? (
-                    <span onClick={()=>{}}>
+                    <span onClick={handleUnlike}>
                         <i className={`fas fa-heart ${styles.Heart}`} />
                     </span>
                 ) : currentUser ? (
-                    <span onClick={(handleLike)=>{}}>
+                    <span onClick={handleLike}>
                         <i className={`far fa-heart ${styles.HeartOutline}`} />
                     </span>
                 ) : (
