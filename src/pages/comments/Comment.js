0426b9073df046f8dcd5d86bb5bb04de8531e1a1
Ben Butler <benbutleranimator@gmail.com>
2024-06-03 15:29:02 +0000

    Add infinite scroll to comments

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,14 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../../styles/Comment.module.css'
 import { Media } from 'react-bootstrap'
 import { Link } from 'react-router-dom/cjs/react-router-dom'
 import Avatar from "../../components/Avatar";
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import { MoreDropdown } from '../../components/MoreDropdown';
+import { axiosRes } from '../../api/axiosDefaults';
+import CommentEditForm from "./CommentEditForm";
+
 
 const Comment = (props) => {
-    const { profile_id, profile_image, owner, updated_at, content } = props;
+    const { 
+        profile_id,
+        profile_image,
+        owner,
+        updated_at,
+        content,
+        id,
+        setPost,
+        setComments,
+    } = props;
 
-  return (
-    <div>
+    const [showEditForm, setShowEditForm] = useState(false);
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+
+
+const handleDelete = async () => {
+    try {
+        await axiosRes.delete(`/comments/${id}`)
+        setPost(prevPost => ({
+            results: [{
+                ...prevPost.results[0],
+                comments_count: prevPost.results[0].comments_count - 1
+            }]
+        }))
+
+        setComments(prevComments => ({
+            ...prevComments,
+            results: prevComments.results.filter((comment) => comment.id !== id),
+        }))
+    } catch(err){
+
+    }
+}
+
+return (
+    <>
       <hr />
       <Media>
         <Link to={`/profiles/${profile_id}`}>
@@ -17,11 +55,28 @@ const Comment = (props) => {
         <Media.Body className="align-self-center ml-2">
           <span className={styles.Owner}>{owner}</span>
           <span className={styles.Date}>{updated_at}</span>
-          <p>{content}</p>
+          {showEditForm ? (
+            <CommentEditForm
+            id={id}
+            profile_id={profile_id}
+            content={content}
+            profileImage={profile_image}
+            setComments={setComments}
+            setShowEditForm={setShowEditForm}
+          />
+          ) : (
+            <p>{content}</p>
+          )}
         </Media.Body>
+        {is_owner && !showEditForm && (
+          <MoreDropdown
+            handleEdit={() => setShowEditForm(true)}
+            handleDelete={handleDelete}
+          />
+        )}
       </Media>
-    </div>
-  )
+    </>
+  );
 }
 
 export default Comment
